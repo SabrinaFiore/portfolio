0ee58be6c7260761df2235c4221971988c466dbf
Apple <apple@MacBook-Pro-di-Apple.local>
2019-04-18 16:56:45 +0200

    add changes on indes.js

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,30 +1,50 @@
 import gulp from 'gulp';
 import rename from 'gulp-rename'; 
-
 import sass from 'gulp-sass';
 import twig from 'gulp-twig';
-
+import sourcemaps from 'gulp-sourcemaps';
+import cssnano from 'gulp-cssnano';
+import autoprefixer from 'gulp-autoprefixer';
 import browserSync from 'browser-sync';
+import yarn from 'gulp-yarn';
 
-gulp.task('style', () => {
-    console.log('execute gulp file');
+const SASS_FILES = `src/scss/assets/styles.scss`;
+const AUTOPREFIXER_ARGS = {
+    browsers: ['last 2 versions'],
+    cascade: false
+};
 
-    return gulp.src('src/assets/scss/styles.scss')
-        .pipe(rename('css/styles.css'))
-        .pipe(gulp.dest('dist'));
+gulp.task('default', function() {
+    return gulp
+        .src(SASS_FILES)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(AUTOPREFIXER_ARGS))
+        .pipe(
+            cssnano({
+                discardComments: { removeAll: true },
+                reduceIndents: false,
+                zIndex: false
+            })
+        )
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('index', () => {
     console.log('execute twig');
 
     return gulp.src('src/markup/index.twig')
-    .pipe(rename('html/index.html'))
-    .pipe(gulp.dest('dist/html'))
+        .pipe(rename('html/index.html'))
+        .pipe(gulp.dest('dist/html'))
 });
 
 
 gulp.task('twig', function() {
-    
     return gulp.src('src/markup/*.twig')
     .pipe(twig())
     .pipe(gulp.dest('dist/html'))
@@ -33,31 +53,29 @@ gulp.task('twig', function() {
     }))
 });
 
-gulp.task('sass', function () {
-    
-    return gulp.src('src/assets/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
+
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', gulp.series('default'));
+    gulp.watch('src/markup/index.twig', gulp.series('twig'));
 });
 
-gulp.task('watch', function(done) {
-    gulp.watch('src/assets/scss/**/*.scss', gulp.task('default'));
-    gulp.watch('src/markup/index.twig', gulp.task('twig'));
-    console.log('task twig and sass works');
-    done();
+
+gulp.task('start', gulp.parallel('watch'));
+
+gulp.task('yarn', function() {
+    return gulp.src(['./package.json'])
+        .pipe(yarn());
 });
 
-gulp.task('browser-sync', () => {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
-            baseDir: 'dist/',
-            browser:"google chrome",
-            open: true,
+            baseDir: './dist/html',
+            routes: {
+                '/dist/assets/css/' : 'assets/css',
+            }
         }
     });
 });
 
-gulp.task('start', gulp.parallel('browser-sync','twig', 'sass', 'watch'));
+gulp.task('yarn-start', gulp.parallel('browser-sync', 'yarn', 'watch'));
