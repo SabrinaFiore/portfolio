4e616936f855261c54015cb3d5743f8c9e0a3e46
Sabrina <sabrinafiore1992@gmail.com>
2020-04-11 12:15:37 +0200

    add layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
