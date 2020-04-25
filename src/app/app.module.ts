4be4d80414f5001270db95142ed1baaca11607dd
Sabrina <sabrinafiore1992@gmail.com>
2020-04-25 15:08:35 +0200

    add app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,43 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ListComponent } from './list/list.component'; // home
+import { DetailsComponent } from './details/details.component'; // works
+import { MapsComponent } from './maps/maps.component';  // photos
+
+import { MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    LayoutComponent
+    LayoutComponent,
+    ListComponent,
+    DetailsComponent,
+    MapsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
