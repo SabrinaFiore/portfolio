ecfcae3e24e08b9051c4157faff5a5ddda53f47b
Sabrina Fiore <sabrinafiore@Sabrinas-MBP.home-life.hub>
2020-11-27 13:04:53 +0100

    claim-description: add info

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  claim: string =  'Welcome to my webpage. I\'m Sabrina, a Multidisciplinary Front-End Developer';
+  claim =  'Welcome to my webpage. I\'m Sabrina, a Multidisciplinary Front-End Developer.';
+  // tslint:disable-next-line: max-line-length
+  claimDescription = 'I would love to share with you my Portfolio developed on my own, through my development experiences, using my photos and also my Design Skills to give you a whole image about myself.';
 
   constructor() { }
 
