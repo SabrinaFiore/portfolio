746e580e7f4a5f46c57c17cd530f5dcc6422d7d3
Sabrina Fiore <sabrinafiore@Sabrinas-MBP.home-life.hub>
2020-11-30 20:00:19 +0100

    case-study: implement array

--- src/app/components/case-study/case-study.component.ts
+++ src/app/components/case-study/case-study.component.ts
@@ -13,15 +13,45 @@ export class CaseStudyComponent implements OnInit {
   styleguide = [
     {
       id: 1,
-      url: 'assets/images/styleguide-01.jpg',
+      title: 'Portfolio',
+      subtitle: 'Design',
+      case: {
+        // tslint:disable-next-line: max-line-length
+        introduction: 'Hey you, welcome! Nice to meet you. I would like to introduce you to the case study of my Portfolio, I had this project in my mind for a long time and I really enjoyed designing and developing it.',
+        concept: 'Concept',
+        description: 'Realize something that could represent me through the photos that I took using my developing skills',
+      },
+      image: [
+        {
+          id: 1,
+          url: 'assets/images/styleguide-01.jpg',
+        },
+        {
+          id: 2,
+          url: 'assets/images/styleguide-02.jpg',
+        },
+        {
+          id: 3,
+          url: 'assets/images/styleguide-03.jpg',
+        },
+      ]
     },
     {
       id: 2,
-      url: 'assets/images/styleguide-02.jpg',
-    },
-    {
-      id: 3,
-      url: 'assets/images/styleguide-03.jpg',
+      title: '',
+      subtitle: 'Development',
+      case: {
+        // tslint:disable-next-line: max-line-length
+        introduction: '',
+        concept: 'Steps',
+        description: 'Realize something that could represent me through the photos that I took using my developing skills',
+      },
+      images: [
+        {
+          id: 3,
+          url: 'assets/images/styleguide-03.jpg',
+        },
+      ]
     }
   ];
 
