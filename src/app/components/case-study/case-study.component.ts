f537dd357e9aa82123f49e4773003d777ac9fa8e
Sabrina Fiore <sabrinafiore@Sabrinas-MBP.home-life.hub>
2020-12-01 13:11:27 +0100

    case-study: add content

--- src/app/components/case-study/case-study.component.ts
+++ src/app/components/case-study/case-study.component.ts
@@ -17,9 +17,10 @@ export class CaseStudyComponent implements OnInit {
       subtitle: 'Design',
       case: {
         // tslint:disable-next-line: max-line-length
-        introduction: 'Hey welcome! Nice to meet you. I would like to introduce you to the case study of my Portfolio, I had this project in my mind for a long time and I really enjoyed designing and developing it.',
+        introduction: 'Hey welcome! Nice to meet you. I would like to share with you the case study of my Portfolio because I really belive in a open-sourse creativity process. I had this project in my mind for a long time and I really enjoyed designing and developing it.',
         concept: 'The Concept:',
-        description: 'Realizing something that could represent me through the photos that I took highlighting my developing skills',
+        // tslint:disable-next-line: max-line-length
+        description: 'Realizing something that could represent me through my designer abilities using the photos that I took around and especially highlighting my developing skills',
       },
       image: [
         {
@@ -39,18 +40,43 @@ export class CaseStudyComponent implements OnInit {
     {
       id: 2,
       title: '',
-      subtitle: 'Development',
+      subtitle: 'UI',
       case: {
         // tslint:disable-next-line: max-line-length
+        introduction: 'First of all I designed the wireframing of the project working with the content, and building up layout and information architecture. Once a framework is chosen, it is necessary to determine how the pages are actually going to look. I wanted a web that looked like a modern-minimal layout where I can implement amazing development tools and libraries. Then, I started laying content designing a grid and put content in a \'Dark mode\' usign a dark color as the primary background color.',
+        concept: 'Process',
+        // tslint:disable-next-line: max-line-length
+        description: '',
+      },
+      image: [
+        {
+          id: 1,
+          url: 'assets/images/ui-home-black.jpg',
+        },
+        {
+          id: 2,
+          url: 'assets/images/ui-home-white.jpg',
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: '',
+      subtitle: 'Development',
+      case: {
         introduction: '',
         concept: 'Steps',
-        description: 'Realize something that could represent me through the photos that I took using my developing skills',
+        description: '',
       },
-      images: [
+      image: [
         {
-          id: 3,
-          url: 'assets/images/styleguide-03.jpg',
+          id: 1,
+          url: '',
         },
+        {
+          id: 2,
+          url: '',
+        }
       ]
     }
   ];
