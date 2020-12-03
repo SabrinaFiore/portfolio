f6e9ba86a2843151521f5f6a8a1a6b68bc0e6205
SabrinaFiore <sabrinafiore1992@gmail.com>
2020-12-03 12:43:32 +0100

    case-study: fix fluid/alternate/layout

--- src/app/components/case-study/case-study.component.ts
+++ src/app/components/case-study/case-study.component.ts
@@ -17,7 +17,7 @@ export class CaseStudyComponent implements OnInit {
       case: {
         // tslint:disable-next-line: max-line-length
         introduction: 'Hey welcome! Nice to meet you. I would like to share with you the case study of my Portfolio because I really belive in a open-sourse creativity process. I had this project in my mind for a long time and I really enjoyed designing and developing it.',
-        concept: 'The Concept:',
+        concept: 'The Concept',
         // tslint:disable-next-line: max-line-length
         description: 'Realizing something that could represent me through my design abilities using the photos that I took around and especially highlighting my developing skills. I startet to study the font-family and the color palette that I wanted to use, like the slides on left.',
       },
@@ -40,7 +40,7 @@ export class CaseStudyComponent implements OnInit {
       id: 2,
       title: '',
       subtitle: 'UI',
-      info: 'Moving yourself up and down, left to right, as well, to view more inside the box',
+      info: 'Moving yourself up and down, left to right inside the box to view more inside the box',
       case: {
         // tslint:disable-next-line: max-line-length
         introduction: 'First of all I designed the wireframing of the project working with the content, and building up layout and information architecture. Once a framework is chosen, it is necessary to determine how the pages are actually going to look. I wanted a web that have a modern-minimal layout where I can deploy amazing tools and libraries. Then, I started laying content designing a grid and put content usign a dark color as the primary background color.',
