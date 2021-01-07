82b9fe58deb67a235213f866240134dc7f386bc6
SabrinaFiore <sabrinafiore1992@gmail.com>
2021-01-07 18:14:54 +0100

    work-vew: review info-description

--- src/app/components/case-study/case-study.component.ts
+++ src/app/components/case-study/case-study.component.ts
@@ -20,10 +20,10 @@ export class CaseStudyComponent implements OnInit {
       title: 'Portfolio',
       subtitle: 'Design',
       info: '',
-      introduction: "Welcome! I would like to share with you the case study of my Portfolio, I had this project in my mind for a long time and I really enjoyed designing and developing it. For these reason and also becouse I belive in a open-sourse creativity process, I created this case-study.",
+      introduction: "Hey you, welcome, I so glad that you are here! I would like to share with you the case study of my Portfolio, I had this project in my mind for a long time and I really enjoyed designing and developing it. I also belive in a open-sourse creativity process, if you require any further information, feel free to contact me.",
       concept: 'The Concept',
       // tslint:disable-next-line: max-line-length
-      description: "Realizing something that could represent me through my design abilities using the photos that I took around and especially highlighting my developing skills. About a one ago, I started with a deep research about 'How should/could be my Porfolio', so I did one 'moonboard' with some inspiration. - Wireframing section - I startet to study the font-family and the color palette that I wanted to use, like the slides on left.",
+      description: "Realizing something that could represent me through my design abilities using the photos that I took around and especially highlighting my developing skills. Finding a solution to put my hobbies and habilities all together was really hard, so I started with deep research about 'How should / could be my Portfolio', so I did one 'mood board' with some inspiration taking from the web. After that, I drew the Wireframing and a basic workflow of my web-page... I don't know for how many times I changed my mind!",
       image: [
         {
           id: 1,
